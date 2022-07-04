92aa43bbf6dfd2c360a28425465e20bc93fd16ee
Rezaofc6 <108604209+Rezaofc6@users.noreply.github.com>
2022-07-04 18:10:25 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,8 @@
 function handler(m) {
   const data = global.owner.filter(([id, isCreator]) => id && isCreator)
   this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
-  this.reply(m.chat, "My Owner", m)
+  this.reply(m.chat, "Nomer Owner Ku Dibawah Ini Kak, Sv Ya Kak Dia Ganteng Loh ><
+Follow Ig Owner Ku :https://instagram.com/itz.reza_official_", m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
